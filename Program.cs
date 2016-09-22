539317327b43b166c80a911cc232effc3faf0672
nshawcroft <nshawcroft@gmail.com>
2016-09-22 17:03:15 -0600

    Nate's initial code

--- Program.cs
+++ Program.cs
@@ -22,6 +22,75 @@ namespace ConsoleApplication2
             sNum = Console.Read();
 
             while(Int32.TryParse(iNum))
+            
+            Console.WriteLine("1. Add one Item to queue");
+            Console.WriteLine("2. Add Huge List of Items to Queue");
+            Console.WriteLine("3. Display Queue");
+            Console.WriteLine("4. Delete from Queue");
+            Console.WriteLine("5. Clear Queue");
+            Console.WriteLine("6. Search Queue");
+            Console.WriteLine("7. Return to Main Menu\n");
+
+            selected = Console.ReadLine();
+
+                if (selected == "1")
+                {
+                    Console.WriteLine("Please enter a string");
+                    qItems.Enqueue(Console.ReadLine());
+                }
+
+                else if (selected == "2")
+                {
+                    qItems.Clear();
+                    int counter = 1;
+                    while (qItems.Count() <= 2000)
+                    {
+                        qItems.Enqueue("New Entry" + counter);
+                        counter++;
+                    }
+                }
+
+                else if (selected == "3")
+                {
+                    foreach (string item in qItems)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+
+                else if (selected == "4")
+                {
+                    string input = "";
+                    Console.Write("Which item do you want to delete?");
+                    input = Console.ReadLine();
+                    
+                }
+
+                else if (selected == "5")
+                {
+                    qItems.Clear();
+                    Console.WriteLine("All items removed");
+                }
+
+                else if (selected == "6") 
+                {
+                    System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+                    string input = "";
+                    Console.Write("Enter Search field: ");
+                    input = Console.ReadLine();
+                    sw.Start();
+                    if (qItems.Contains(input))
+                    {
+                        sw.Stop();
+                        Console.WriteLine("Found It! Time to find: " + sw.Elapsed);
+                        sw.Reset();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not found");
+                        sw.Reset();
+                    }
+                }
         }
     }
 }
