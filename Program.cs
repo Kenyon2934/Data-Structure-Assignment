a94945c597c80def54e49b4ca221026bb199984b
batchelb <blakebatchelor2@gmail.com>
2016-09-22 15:52:53 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iNum;
+            string 
+
+            Console.WriteLine("Enter your Options\n");
+            Console.WriteLine("1. Stack");
+            Console.WriteLine("2. Queue");
+            Console.WriteLine("3. Dictionary");
+            Console.WriteLine("4. Exit\n");
+
+            sNum = Console.Read();
+
+            while(Int32.TryParse(iNum))
+        }
+    }
+}
